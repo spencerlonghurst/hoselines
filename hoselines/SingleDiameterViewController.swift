35390db726f4d175cb53e53dbb6d1172a7cf4f5b
Spencer Longhurst <spencersean12@gmail.com>
2023-05-02 17:55:51 -0600

    Added everything for the Smooth Tip Size Picker

--- hoselines/SingleDiameterViewController.swift
+++ hoselines/SingleDiameterViewController.swift
@@ -13,6 +13,7 @@ class SingleDiameterViewController: UIViewController {
     var HoseLength = 50.0
 
     let diameterSizes = ["1.5", "1.75", "2.5", "3", "4", "5" ]
+    let smoothTipSizes = ["1/2", "5/8", "3/4", "7/8", "15/16", "1", "1 1/8", "1 1/4", "1 3/8", "1 1/2", "1 3/4", "2", "2 1/4", "2 1/2", "2 3/4", "3"]
 
     @IBOutlet weak var SingleDiameterPicker: UIPickerView!
     @IBOutlet weak var SingleLengthLabel: UILabel!
@@ -22,12 +23,22 @@ class SingleDiameterViewController: UIViewController {
         print(HoseLength)
     }
     
+    @IBOutlet weak var SingleSegment: UISegmentedControl!
+    
+    @IBAction func SingleSegmentAction(_ sender: UISegmentedControl) {
+    }
+    
+    @IBOutlet weak var SingleSmoothPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Single!")
         
         SingleDiameterPicker.dataSource = self
         SingleDiameterPicker.delegate = self
+        
+        SingleSmoothPicker.dataSource = self
+        SingleSmoothPicker.delegate = self
     }
     
     
@@ -41,18 +52,25 @@ extension SingleDiameterViewController: UIPickerViewDataSource {
     }
 
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return diameterSizes.count
+        if pickerView == SingleDiameterPicker {
+            return diameterSizes.count
+        } else if pickerView == SingleSmoothPicker {
+            return smoothTipSizes.count
+        } else {
+            return diameterSizes.count
+        }
     }
-
-
 }
 
 extension SingleDiameterViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        print(diameterSizes[row])
-//        HoseDiameter = SingleDiameterPicker!
-        return diameterSizes[row]
-
+        
+        if pickerView == SingleDiameterPicker {
+            return diameterSizes[row]
+        } else if pickerView == SingleSmoothPicker {
+            return smoothTipSizes[row]
+        } else {
+            return diameterSizes[row]
+        }
     }
-    
 }
