96a6da57aa1181546322f05316b0d98fe62f8ae6
Spencer Longhurst <spencersean12@gmail.com>
2023-05-03 17:42:49 -0600

    Added App image and made the home screen styling look a lot better

--- hoselines/ViewController.swift
+++ hoselines/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class HomeView: UIViewController {
 
+    @IBOutlet weak var HomeSingle: UIButton!
+    @IBOutlet weak var HomeStep: UIButton!
+    @IBOutlet weak var HomeDeck: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Home!")
+        
+        HomeSingle.layer.cornerRadius = 10
+        HomeStep.layer.cornerRadius = 10
+        HomeDeck.layer.cornerRadius = 10
     }
 
 
