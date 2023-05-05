2de12bd5d2319eeb9e4087002d743388420d540d
Spencer Longhurst <spencersean12@gmail.com>
2023-05-04 21:16:50 -0600

    Removed all the zeros from the numbers and created the friction loss function with hard coded values

--- hoselines/SingleDiameterViewController.swift
+++ hoselines/SingleDiameterViewController.swift
@@ -9,18 +9,25 @@ import UIKit
 
 class SingleDiameterViewController: UIViewController {
     
-    var HoseDiameter = 1.50
+//    var HoseDiameter: Double
     var HoseLength = 50.0
 
     let diameterSizes = ["1.5", "1.75", "2.5", "3", "4", "5" ]
     let smoothTipSizes = ["1/2", "5/8", "3/4", "7/8", "15/16", "1", "1 1/8", "1 1/4", "1 3/8", "1 1/2", "1 3/4", "2", "2 1/4", "2 1/2", "2 3/4", "3"]
+    
+    
+    
+    
+    
+    
 
     @IBOutlet weak var SingleDiameterPicker: UIPickerView!
     @IBOutlet weak var SingleLengthLabel: UILabel!
     @IBAction func SingleLengthStepper(_ sender: UIStepper) {
-        SingleLengthLabel.text = String(sender.value)
-        HoseLength = sender.value
-        print(HoseLength)
+        SingleLengthLabel.text = "\(Int(sender.value)) Feet"
+        
+//        HoseLength = sender.value
+//        print(HoseLength)
     }
     
     @IBOutlet weak var SingleSegment: UISegmentedControl!
@@ -47,33 +54,44 @@ class SingleDiameterViewController: UIViewController {
     @IBOutlet weak var SingleSmoothPicker: UIPickerView!
     
 
-    let potato = 0
     
     @IBOutlet weak var SinglePressureLabel: UILabel!
     @IBAction func SinglePressureStepper(_ sender: UIStepper) {
-        SinglePressureLabel.text = String(sender.value)
+        SinglePressureLabel.text = "\(Int(sender.value)) PSI"
     }
     
     
     @IBOutlet weak var SingleGPMLabel: UILabel!
     
     @IBAction func SingleGPMStepper(_ sender: UIStepper) {
-        SingleGPMLabel.text = String(sender.value)
+        SingleGPMLabel.text = "\(Int(sender.value)) GPM"
     }
     
     @IBOutlet weak var SingleElevationLabel: UILabel!
     
     @IBAction func SingleElevationStepper(_ sender: UIStepper) {
-        SingleElevationLabel.text = String(sender.value)
+        SingleElevationLabel.text = "\(Int(sender.value)) Feet"
     }
     
     
+    @IBOutlet weak var FrictionLoss: UILabel!
     
     
+    @IBOutlet weak var PumpPressure: UILabel!
     
-    
-    
-    
+    @IBAction func FrictionLossButton(_ sender: UIButton) {
+        func frictionLoss(coefficent: Double, flowRate: Double, hoseLength: Double){
+//            let FL = coefficent + flowRate + hoseLength
+            let A = coefficent
+            let B = ((flowRate/100)*(flowRate/100))
+            let C = (hoseLength/100)
+            let FL = Int(A * B * C)
+            print(FL)
+            FrictionLoss.text = "\(FL) PSI Loss"
+        }
+        
+        frictionLoss(coefficent: 15.5, flowRate: 250, hoseLength: 100)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -85,10 +103,11 @@ class SingleDiameterViewController: UIViewController {
         SingleSmoothPicker.dataSource = self
         SingleSmoothPicker.delegate = self
         
-        SinglePressureLabel.text = "75"
-        SingleGPMLabel.text = "100"
+//        print(SingleDiameterPicker)
         
     }
+    
+    
     
     
 
@@ -115,6 +134,8 @@ extension SingleDiameterViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
         if pickerView == SingleDiameterPicker {
+//            HoseDiameter = Double(diameterSizes[row])!
+//            print(HoseDiameter)
             return diameterSizes[row]
         } else if pickerView == SingleSmoothPicker {
             return smoothTipSizes[row]
