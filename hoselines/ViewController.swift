f131bc5282587bac6e0f5bc50454665011ff5c55
Spencer Longhurst <spencersean12@gmail.com>
2023-05-08 20:14:01 -0600

    Prepared for App Store deployment

--- hoselines/ViewController.swift
+++ hoselines/ViewController.swift
@@ -10,22 +10,13 @@ import UIKit
 class HomeView: UIViewController {
 
     @IBOutlet weak var HomeSingle: UIButton!
-    @IBOutlet weak var HomeStep: UIButton!
-    @IBOutlet weak var HomeDeck: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Home!")
         
         HomeSingle.layer.cornerRadius = 10
-        HomeStep.layer.cornerRadius = 10
-        HomeDeck.layer.cornerRadius = 10
-        
-        HomeStep.isUserInteractionEnabled = false
-        HomeStep.alpha = 0.5
-        
-        HomeDeck.isUserInteractionEnabled = false
-        HomeDeck.alpha = 0.5
+
+ 
     }
 
 
